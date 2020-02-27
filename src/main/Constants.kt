331afac9e9424f169efde8cfeb3db5dab1c85929
UrielSarrazin <uriel.sarrazin@digixall.io>
2020-02-27 15:41:44 +0100

    fixing unit tests

--- src/main/Constants.kt
+++ src/main/Constants.kt
@@ -1,9 +1,9 @@
 class Constants {
     companion object Interval {
-        const val UNISON = 1
-        const val MINOR_THIRD = 4
-        const val MAJOR_THIRD = 5
-        const val PERFECT_FIFTH = 8
-        const val OCTAVE = 13
+        const val UNISON = 0
+        const val MINOR_THIRD = 3
+        const val MAJOR_THIRD = 4
+        const val PERFECT_FIFTH = 7
+        const val OCTAVE = 12
     }
 }
